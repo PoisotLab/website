8cadf1389d8e09eb56cf828979147d7a9d9b010f
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-09-04 09:56:59 -0400

    update

--- static/ui/main.js
+++ static/ui/main.js
@@ -14,7 +14,9 @@ window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos == 0) {
         document.getElementById("topbar").classList.remove("scrolled");
+        document.getElementById("logo-navbar").src="logo/colour-white.png";
     } else {
         document.getElementById("topbar").classList.add("scrolled");
+        document.getElementById("logo-navbar").src="logo/colour-black.png";
     }
 } 
