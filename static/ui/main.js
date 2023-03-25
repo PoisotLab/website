03453692fe378c94562cf7c9ed49ed3185425c5b
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-03-25 18:08:20 -0400

    fix weird behavior of menu on mobile

--- static/ui/main.js
+++ static/ui/main.js
@@ -1,12 +1,21 @@
 function toggleMainMenu() {
     var nav = document.getElementById("menu");
     var tbar = document.getElementById("topbar");
+    var currentScrollPos = window.pageYOffset;
     if (nav.style.display === "block") {
         nav.style.display = "none";
-        tbar.classList.toggle("scrolled");
+        if (tbar.classList.contains("scrolled")) {
+            tbar.classList.remove("scrolled");
+        }
     } else {
         nav.style.display = "block";
-        tbar.classList.toggle("scrolled");
+        if (!(tbar.classList.contains("scrolled"))) {
+            tbar.classList.add("scrolled");
+        }
+    }
+    if (currentScrollPos != 0) {
+        document.getElementById("topbar").classList.add("scrolled");
+        document.getElementById("logo-navbar").src="logo/colour-black.png";
     }
 }
 
