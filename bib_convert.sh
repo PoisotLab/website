9868f22ec02a0319932b5fee1a9c5b24ae9147fc
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-06-14 12:39:55 -0400

    new section

--- bib_convert.sh
+++ bib_convert.sh
@@ -1,4 +1,4 @@
-for collection in BiologicalMonitoring IndependentResearch CoEvolution EltonianShortfall NetworksInSpace InteractionPrediction ViromeWork Collaborations MicrobialSystems NetworkStructure NetworkVariability SustainableResearch
+for collection in BiologicalMonitoring IndependentResearch CoEvolution EltonianShortfall NetworksInSpace InteractionPrediction ViromeWork Collaborations MicrobialSystems NetworkStructure NetworkVariability SustainableResearch CommunityEcology
 do
     pandoc references/$collection.bib -t csljson -o data/bibliographies/$collection.json
 done
---
+++ content/research/CommunityEcology.md
@@ -0,0 +1,8 @@
+---
+title: Community ecology
+weight: 1
+---
+
+When species interact, do they evolve differently? When species evolve, do they keep the same interactions? We use data analysis and simulation to explore some of these questions at the scale of ecological networks.
+
+<!--more-->
